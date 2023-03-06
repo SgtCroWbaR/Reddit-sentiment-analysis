38f63979574f406c07e3f71af37a7e0ae9d6b666
Vessely <sgt.crowbar@gmail.com>
2023-03-06 22:38:15 +0100

    Added GloVe word embeddings

---
+++ word_embeddings/__init__.py
@@ -0,0 +1,18 @@
+import numpy as np
+from typing import List
+from copy import deepcopy
+
+
+class Embeddings:
+    def __init__(self, texts: List[str], dictionary: dict):
+        self.texts = texts
+        self.dictionary = dictionary
+        self.texts_embedings = []
+        self._compute_embeddings()
+
+    def _compute_embeddings(self):
+        for comment in self.texts:
+            comment_embeddings = []
+            for word in comment.strip().split(' '):
+                comment_embeddings.append(np.array(self.dictionary.get(word, np.zeros((100,))), dtype=float))
+            self.texts_embedings.append(deepcopy(np.array(comment_embeddings)))
