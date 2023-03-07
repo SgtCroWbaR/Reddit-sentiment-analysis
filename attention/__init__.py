814470b0c9e41ac786b83fbda0a47d7021b46885
Vessely <sgt.crowbar@gmail.com>
2023-03-07 12:58:35 +0100

    Removed magic constant

--- attention/__init__.py
+++ attention/__init__.py
@@ -5,9 +5,10 @@ from math import ceil
 
 class MultiHeadAttention:
 
-    def __init__(self, embeddings_array, num_of_heads):
+    def __init__(self, embeddings_array, num_of_heads, input_dim=100):
         self.embeddings_array = embeddings_array
         self.num_of_heads = num_of_heads
+        self.input_dim = input_dim
         self._calc_attention()
 
     def _calc_attention(self):
@@ -15,7 +16,7 @@ class MultiHeadAttention:
         self.normalised_scores = []
         self.reweighted = []
 
-        interval = ceil(100 / self.num_of_heads)
+        interval = ceil(self.input_dim / self.num_of_heads)
         for embeddnings in self.embeddings_array:
             i = 0
             score = np.dot(embeddnings[:, i * interval:(i + 1) * interval],
